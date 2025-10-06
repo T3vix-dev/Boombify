6c6824f8ac41e0d255efa8e8c8c19ec6ded288ed
natalia506 <kiana.natalia@strathmore.edu>
2025-10-06 22:23:49 +0300

    Update boombify.py

--- boombify.py
+++ boombify.py
@@ -3,4 +3,9 @@ import customtkinter as ctk
 from tkinter import *
 from PIL import Image, ImageTk
 # App setup
+ctk.set_appearance_mode("dark") # Dark mode for modern look
+ctk.set_default_color_theme("green")#Spotify-like green
+app= ctk.CTk()
+app.title("Boombify🎵")
+app.geometry("1100*650")
 
